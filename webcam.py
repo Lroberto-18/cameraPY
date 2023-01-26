758e8438cfc48a5371513969badfd1abcd5f85af
Leandro <80781574+Lroberto-18@users.noreply.github.com>
2023-01-26 18:24:16 -0300

    Add files via upload

---
+++ webcam.py
@@ -0,0 +1,16 @@
+import cv2
+
+capture = cv2.VideoCapture(0)
+i = 0
+while(True):
+    ret, frame = capture.read()
+    frame = cv2.flip(frame,1)
+    cv2.imshow("WEBCAM", frame)   
+    k = cv2.waitKey(1)
+    if k==32:
+        i+=1
+        cv2.imwrite(F'Caputura_{i}.png', frame)
+    if k==27:
+        break
+capture.release()
+cv2.destroyAllWindows()
