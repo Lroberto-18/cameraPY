97096e06ebd4b7772d227bf12437e000ef1c7aae
Leandro <80781574+Lroberto-18@users.noreply.github.com>
2023-02-09 10:16:17 -0300

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,16 +1,20 @@
 import cv2
 
-capture = cv2.VideoCapture(0)
-i = 0
-while(True):
-    ret, frame = capture.read()
-    frame = cv2.flip(frame,1)
-    cv2.imshow("WEBCAM", frame)   
-    k = cv2.waitKey(1)
-    if k==32:
-        i+=1
-        cv2.imwrite(F'Caputura_{i}.png', frame)
-    if k==27:
-        break
-capture.release()
-cv2.destroyAllWindows()
+def main():
+    capture = cv2.VideoCapture(0)
+    i = 0
+    while(True):
+        ret, frame = capture.read()
+        frame = cv2.flip(frame,1)
+        cv2.imshow("WEBCAM", frame)   
+        k = cv2.waitKey(1)
+        if k==32:
+            i+=1
+            cv2.imwrite(F'Caputura_{i}.png', frame)
+        if k==27:
+            break
+    capture.release()
+    cv2.destroyAllWindows()
+    
+if __init__=='__main__':
+    main()
